76fe0713d58e5bdb8c8e16f9bdbbc9440f42b8c2
TonimaDristy <tidristy477@gmail.com>
2025-12-05 13:54:13 +0600

    JA JB

--- JA JB.asm
+++ JA JB.asm
@@ -1,3 +1,9 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
 .MODEL SMALL
 .STACK 100H
 .DATA
@@ -54,4 +60,10 @@ EXIT:
     INT 21H
 
 END MAIN
+
+
+ret
+
+
+
 
