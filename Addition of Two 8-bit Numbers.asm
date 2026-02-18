7fa5132e595932494f8034b2328595f40f2f8456
TonimaDristy <tidristy477@gmail.com>
2026-02-18 22:28:02 +0600

    Addition of Two 8-bit Numbers

---
+++ Addition of Two 8-bit Numbers.asm
@@ -0,0 +1,31 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+.model small
+.stack 100h
+.data
+    num1 db 05h
+    num2 db 03h
+    result db ?
+.code
+main:
+    mov ax, @data
+    mov ds, ax
+
+    mov al, num1
+    add al, num2
+    mov result, al
+
+    mov ah, 4ch
+    int 21h
+end main
+
+
+ret
+
+
+
+
