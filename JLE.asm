1630707d8c9bd8e74419c94f6ba440c44dd99eac
TonimaDristy <tidristy477@gmail.com>
2026-04-04 22:59:25 +0600

    Negative Numbers in Array

--- JLE.asm
+++ JLE.asm
@@ -1,32 +1,41 @@
 .MODEL SMALL
 .STACK 100H
 .DATA
-    NUM DB -3
+    ARR DB -1, 5, -3, 2, -7
+    COUNT DB 5
+    NEG_CNT DB 0
 
-    negMsg DB "Negative (SF=1)$"
-    posMsg DB "Positive (SF=0)$"
+    msg DB "Negative count calculated$"
 
 .CODE
 MAIN PROC
     MOV AX, @DATA
     MOV DS, AX
 
-    MOV AL, NUM
-    OR AL, 0       ; update flags without changing value
+    MOV CX, COUNT
+    LEA SI, ARR
+    XOR BL, BL        ; counter = 0
 
-    JS NEGATIVE    ; Jump if Sign Flag = 1
+CHECK_LOOP:
+    MOV AL, [SI]
+    OR AL, AL         ; set flags
 
-    LEA DX, posMsg
+    JS IS_NEG
+    JMP NEXT
+
+IS_NEG:
+    INC BL
+
+NEXT:
+    INC SI
+    LOOP CHECK_LOOP
+
+    MOV NEG_CNT, BL
+
+    LEA DX, msg
     MOV AH, 9
     INT 21H
-    JMP EXIT
 
-NEGATIVE:
-    LEA DX, negMsg
-    MOV AH, 9
-    INT 21H
-
-EXIT:
     MOV AH, 4CH
     INT 21H
 END MAIN
