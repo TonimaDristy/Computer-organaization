83a1b27807a9429e392f4518f11e74374a4d58c4
TonimaDristy <tidristy477@gmail.com>
2026-04-05 22:27:19 +0600

    List is Sorted

--- JLE.asm
+++ JLE.asm
@@ -1,41 +1,10 @@
-.MODEL SMALL
-.STACK 100H
-.DATA
-    ARR DB -1, 5, -3, 2, -7
-    COUNT DB 5
-    NEG_CNT DB 0
+def main():
+    numbers = list(map(int, input("Enter numbers separated by space: ").split()))
 
-    msg DB "Negative count calculated$"
+    if numbers == sorted(numbers):
+        print("List is sorted")
+    else:
+        print("List is not sorted")
 
-.CODE
-MAIN PROC
-    MOV AX, @DATA
-    MOV DS, AX
-
-    MOV CX, COUNT
-    LEA SI, ARR
-    XOR BL, BL        ; counter = 0
-
-CHECK_LOOP:
-    MOV AL, [SI]
-    OR AL, AL         ; set flags
-
-    JS IS_NEG
-    JMP NEXT
-
-IS_NEG:
-    INC BL
-
-NEXT:
-    INC SI
-    LOOP CHECK_LOOP
-
-    MOV NEG_CNT, BL
-
-    LEA DX, msg
-    MOV AH, 9
-    INT 21H
-
-    MOV AH, 4CH
-    INT 21H
-END MAIN
+if __name__ == "__main__":
+    main()
